9b733f6aa8004cbc78047587235e1546047fd2b5
MadfixIsMe <p.lukac47@gmail.com>
2025-11-23 21:45:14 +0100

    Edit2 (Check Desc.)
    - Background hry (game.tscn, card_manager.gd) + pridané sprites.
    - Card3 card_3.tscn
    - náhodné karty každú hru (card_manager.gd)

--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -2,9 +2,43 @@ extends Node2D
 
 var screen_size
 var card_being_dragged: Node2D
+<<<<<<< Updated upstream
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+=======
+var home_positions := {} #dictionary pre karty a ich pozície
+var random = RandomNumberGenerator.new()
+var bottom_positions = [Vector2(230, 555), Vector2(340, 555)]
+
+func rand_cards_pick(count := 2): #kolko kariet chceme
+	var cards = get_children()
+	for child in get_children():
+		if child.name.begins_with("Card"):
+			cards.append(child)
+	var selected_cards = []
+	var cardnum = random.randi_range(0, 1) #podla mnostva kariet 2 hodnoty 0, 1
+	
+	random.randomize()
+	
+	for i in range(count):
+		selected_cards.append(cards[cardnum])
+		cards.remove_at(cardnum)
+	return selected_cards
+
+func deal_cards():
+	var picked_cards = rand_cards_pick()
+	for i in picked_cards.size():
+		var card = picked_cards[i]
+		card.visible = true
+		card.position = bottom_positions[i]
+		home_positions[card] = bottom_positions[i]
+		
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	deal_cards()
+>>>>>>> Stashed changes
 
 func _process(delta: float) -> void:
 	if card_being_dragged:
@@ -19,6 +53,11 @@ func _input(event):
 			if card:
 				card_being_dragged = card
 		else:
+<<<<<<< Updated upstream
+=======
+			if card_being_dragged != null and home_positions.has(card_being_dragged):  #vráti sa naspäť
+				card_being_dragged.position = home_positions[card_being_dragged]
+>>>>>>> Stashed changes
 			card_being_dragged = null
 
 

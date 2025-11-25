f7376e70d4a53dadd5128d8344fbdeb0de55c8b3
MadfixIsMe <p.lukac47@gmail.com>
2025-11-25 14:02:50 +0100

    Add background and random card selection to game
    Introduced a new background sprite to the game scene and updated card positions. Implemented a random_card function in card_manager.gd to randomly select and display two cards at specific positions on game start. Added Dungeon Tile Set sprite and import files.

--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -3,11 +3,37 @@ extends Node2D
 var screen_size
 var card_being_dragged: Node2D
 var home_positions := {} #dictionary pre karty a ich pozície
+var card_base_position = [Vector2(475, 525), Vector2(580, 525)]
+var random = RandomNumberGenerator.new()
+
+func random_card(count := 2): # kolko kariet = count
+	random.randomize()
+	var cards := []
+	var random_cards := []
+	
+	for card in get_children(): #nahrá všetky karty.
+		if card.name.begins_with("Card"):
+			cards.append(card)
+			
+	cards.shuffle() #zamieša list
+	
+	for a in range(count): #vyberie prvé 2.
+		random_cards.append(cards[a])
+		
+	for b in range(random_cards.size()): #zviditelní karty a nastaví ich pozíciu
+		var card = random_cards[b]
+		card.visible = true
+		card.position = card_base_position[b]
+		home_positions[card] = card_base_position[b]
+	
+	return random_cards
+	
+
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	for card in get_children():
-		home_positions[card] = card.position #select všetky začiatočné pozície kariet
+	random_card()
+
 
 func _process(delta: float) -> void:
 	if card_being_dragged:

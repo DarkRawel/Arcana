5fb93450f8643e17ace88e685df6fcef390d4e71
MadfixIsMe <p.lukac47@gmail.com>
2025-11-27 15:12:55 +0100

    Refactor card system to use CardData resource
    Replaced individual card scenes and scripts with a unified Card scene and CardData resource. Updated game scene to instantiate only the new Card scene. Added CardData.gd script and resource, and placeholder art assets. This simplifies card management and enables dynamic card data assignment.

--- scripts/card_1.gd
+++ scripts/card_1.gd
@@ -3,9 +3,21 @@ extends Node2D
 signal hovered
 signal hovered_off
 
+@export var data: CardData
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_parent().connect_card_signals(self) # vola na rodica co je cardmanager
+	if data:
+		apply_data()
+		
+func apply_data():
+	$NameLabel.text = data.name
+	$CostLabel.text = str(data.cost)
+	$DamageLabel.text = str(data.damage)
+	$Description.text = str(data.description)
+	$Sprite2D.texture = data.art
+	
 
 func _on_area_2d_mouse_entered() -> void: # ked sa myska dotkne area2d tak posle signale
 	emit_signal("hovered", self)
